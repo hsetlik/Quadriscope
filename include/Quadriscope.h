b72a2e40d9200eefff47cba9d66eae4e47b88f28
Hayden Setlik <haydensetlik@gmail.com>
2024-02-17 15:46:01 -0700

    buttons, pixels

--- include/Quadriscope.h
+++ include/Quadriscope.h
@@ -6,9 +6,13 @@
 #include <RotaryEncoder.h>
 #include <functional>
 #include <OneButton.h>
+#include <Adafruit_NeoPixel.h>
 using EncoderCallback = std::function<void(bool)>;
 
 #define FRAME_LENGTH_MS 41
+
+#define DISPLAY_W 360
+#define DISPLAY_H 240
 
 class Encoder
 {
@@ -22,6 +26,16 @@ public:
 };
 //===================================================
 
+// store the state and display data for each channel
+struct ScopeChannel
+{
+    uint8_t index = 0;
+    bool active = false;
+    float vAmplitude = 12.0f; // vertical scale, in terms of voltage at the top of the display
+    uint16_t hWindow = 50; // the length of time in ms represented across the width of the display
+    int16_t screenBuffer[DISPLAY_W]; //store a voltage value for each pixel to use all of the display's resolution
+};
+//===================================================
 class Quadriscope // represents The Device
 {
 private:
@@ -34,6 +48,11 @@ private:
     OneButton* b2;
     OneButton* b3;
     OneButton* b4;
+    Adafruit_NeoPixel* pixels;
+// the state that gets modified
+    uint8_t selectedChannel;
+    ScopeChannel channels[4];
+// the dac and its associated data
 // these boys get wired into the encoder and button callbacks
     void leftEncTurn(bool up);
     void rightEncTurn(bool up);
@@ -41,6 +60,12 @@ private:
     void buttonHold(uint8_t idx);
 // clears the display and draws the appropriate waves
     void drawFrame();
+// update the NeoPixels for the current state
+    void updatePixels();
+    uint32_t fromRGB(uint8_t r, uint8_t g, uint8_t b)
+    {
+        return ((uint32_t)b) & ((uint32_t)g << 8) & ((uint32_t)r << 16);
+    }
 public: // main.cpp should only have to touch this code from here down
     Quadriscope();
     ~Quadriscope();
--- src/Quadriscope.cpp
+++ src/Quadriscope.cpp
@@ -1,6 +1,4 @@
 #include "Quadriscope.h"
-
-
 Encoder::Encoder(uint8_t l, uint8_t r) : position(0), enc(l, r, RotaryEncoder::LatchMode::FOUR3)
 {
 
@@ -25,13 +23,16 @@ rEnc(nullptr),
 b1(nullptr),
 b2(nullptr),
 b3(nullptr),
-b4(nullptr)
+b4(nullptr),
+pixels(nullptr),
+selectedChannel(0)
 {
 
 }
 
 Quadriscope::~Quadriscope()
 {
+    // deal with all those manual heap allocations from init()
     delete display;
     delete rEnc;
     delete lEnc;
@@ -39,12 +40,13 @@ Quadriscope::~Quadriscope()
     delete b2;
     delete b3;
     delete b4;
+    delete pixels;
 }
 
 void Quadriscope::init()
 {
     Serial.begin(9600);
-    //step 1: do all our heap allocations
+
     //display
     display = new Adafruit_ILI9341(D_CS, D_DCRC, D_MOSI, D_SCK, D_RST, D_MISO);
     display->begin();
@@ -77,6 +79,16 @@ void Quadriscope::init()
     b3->attachLongPressStop([](void* q){((Quadriscope*)q)->buttonHold(2);}, this);
     b4->attachLongPressStop([](void* q){((Quadriscope*)q)->buttonHold(3);}, this);
 
+    //NeoPixels
+    pixels = new Adafruit_NeoPixel(4, PIXELS, NEO_GRB + NEO_KHZ800);
+    pixels->begin();
+
+    //now initialize the state
+    for(int i = 0; i < 4; i ++)
+    {
+        auto& c = channels[i];
+        c.index = (uint8_t)i;
+    }
 }
 
 void Quadriscope::tick()
@@ -84,6 +96,12 @@ void Quadriscope::tick()
     unsigned long now = millis();
     lEnc->tick();
     rEnc->tick();
+
+    b1->tick();
+    b2->tick();
+    b3->tick();
+    b4->tick();
+
     if(now - prevUpdateMs >= FRAME_LENGTH_MS)
     {
         prevUpdateMs = now;
@@ -94,6 +112,25 @@ void Quadriscope::tick()
 void Quadriscope::drawFrame()
 {
     //TODO
+}
+
+void Quadriscope::updatePixels()
+{
+    //designated colors for each channel
+    static const uint32_t selColors[] = {fromRGB(255, 138, 51), fromRGB(130, 194, 65), fromRGB(133, 213, 255), fromRGB(188, 145, 209)};
+    static const uint32_t onColors[] = {fromRGB(191, 104, 38), fromRGB(74, 134, 13), fromRGB(77, 151, 191), fromRGB(108, 54, 134)};
+    pixels->clear();
+    pixels->setBrightness(60);
+    for(uint8_t i = 0; i < 4; i++)
+    {
+        if(selectedChannel == i)
+            pixels->setPixelColor(i, selColors[i]);
+        else if(channels[i].active)
+            pixels->setPixelColor(i, onColors[i]);
+        else
+            pixels->setPixelColor(i, fromRGB(0, 0, 0));
+    }
+    pixels->show();
 }
 //==========================================================================
 void Quadriscope::leftEncTurn(bool up)
